36268e3f1dad81da6868add29082e78d5f486177
Wesley Zhuang <wzhuang22@d-e.org>
2021-10-19 17:12:39 -0400

    i utilize robs code for gyro turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroAuton.java
@@ -32,6 +32,17 @@ public class GyroAuton extends OpMode {
     private DcMotor backLeft = null;
     private DcMotor frontLeft = null;
     int x;
+    final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+
+
+    // These constants define the desired driving/control characteristics
+    // The can/should be tweaked to suite the specific robot drive train
+
+    final double     HEADING_THRESHOLD       = 5 ;      // As tight as we can make it with an integer gyro
+    double     P_TURN_COEFF            = 0.005;     // Larger is more responsive, but also less stable
+
 
     public void init() {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -48,6 +59,12 @@ public class GyroAuton extends OpMode {
         backRight.setDirection(DcMotor.Direction.FORWARD);
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         telemetry.addLine("Ready.");
         telemetry.update();
@@ -71,14 +88,6 @@ public class GyroAuton extends OpMode {
 
     public void loop() {
 
-//        if (imu.isGyroCalibrated() && calib) {
-//            BNO055IMU.CalibrationData calibrationData = imu.readCalibrationData();
-//            String filename = "BNO055IMUCalibration.json";
-//            File file = AppUtil.getInstance().getSettingsFile(filename);
-//            ReadWriteFile.writeFile(file, calibrationData.serialize());
-//            telemetry.log().add("saved to '%s'", filename);
-//            calib = false;
-//        }
 
         if (gamepad1.a) {
             x = 0;
@@ -115,20 +124,6 @@ public class GyroAuton extends OpMode {
     // HardwarePushbot robot   = new HardwarePushbot();   // Use a Pushbot's hardware
     // ModernRoboticsI2cGyro gyro    = null;                    // Additional Gyro device
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-
-    // These constants define the desired driving/control characteristics
-    // The can/should be tweaked to suite the specific robot drive train.
-    static final double     DRIVE_SPEED             = 0.7;     // Nominal speed for better accuracy.
-    static final double     TURN_SPEED              = 0.5;     // Nominal half speed for better accuracy.
-
-    static final double     HEADING_THRESHOLD       = 5 ;      // As tight as we can make it with an integer gyro
-    static  double     P_TURN_COEFF            = 0.15;     // Larger is more responsive, but also less stable
-    static final double     P_DRIVE_COEFF           = 0.15;     // Larger is more responsive, but also less stable
 
     /**
      *  Method to spin on central axis to point in a new direction.
@@ -141,14 +136,6 @@ public class GyroAuton extends OpMode {
      *                   0 = fwd. +ve is CCW from fwd. -ve is CW from forward.
      *                   If a relative angle is required, add/subtract from current heading.
      */
-    public void gyroTurn (  double speed, double angle) {
-
-        // keep looping while we are still active, and not on heading.
-        while (!onHeading(speed, angle, P_TURN_COEFF)) {
-            // Update telemetry & Allow time for other processes to run.
-            telemetry.update();
-        }
-    }
 
     /**
      *  Method to obtain & hold a heading for a finite amount of time
@@ -160,24 +147,7 @@ public class GyroAuton extends OpMode {
      *                   If a relative angle is required, add/subtract from current heading.
      * @param holdTime   Length of time (in seconds) to hold the specified heading.
      */
-    public void gyroHold( double speed, double angle, double holdTime) {
 
-        ElapsedTime holdTimer = new ElapsedTime();
-
-        // keep looping while we have time remaining.
-        holdTimer.reset();
-        while (holdTimer.time() < holdTime) {
-            // Update telemetry & Allow time for other processes to run.
-            onHeading(speed, angle, P_TURN_COEFF);
-            telemetry.update();
-        }
-
-        // Stop all motion;
-        frontLeft.setPower(0);
-        backLeft.setPower(0);
-        frontRight.setPower(0);
-        backRight.setPower(0);
-    }
 
     /**
      * Perform one cycle of closed loop heading control.
@@ -190,23 +160,25 @@ public class GyroAuton extends OpMode {
      * @return
      */
     boolean onHeading(double speed, double angle, double PCoeff) {
-        double   error ;
+        double actual = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
         double   steer ;
         boolean  onTarget = false ;
         double leftSpeed;
         double rightSpeed;
+        double error =  (angle - actual + 360.0) % 360.0;
 
         // determine turn power based on +/- error
-        error = getError(angle);
+
 
         if (Math.abs(error) <= HEADING_THRESHOLD) {
             steer = 0.0;
             leftSpeed  = 0.0;
             rightSpeed = 0.0;
             onTarget = true;
+
         }
         else {
-            steer = getSteer(error, PCoeff);
+            steer = gyroCorrect(angle, 2, actual , 0.05, 0.3);
             rightSpeed  = speed * steer;
             leftSpeed   = -rightSpeed;
         }
@@ -216,6 +188,7 @@ public class GyroAuton extends OpMode {
         backLeft.setPower(leftSpeed);
         frontRight.setPower(rightSpeed);
         backRight.setPower(rightSpeed);
+
 
         // Display it for the driver.
         telemetry.addData("Target", "%5.2f", angle);
@@ -232,33 +205,28 @@ public class GyroAuton extends OpMode {
      * @return  error angle: Degrees in the range +/- 180. Centered on the robot's frame of reference
      *          +ve error means the robot should turn LEFT (CCW) to reduce error.
      */
-    public double getError(double targetAngle) {
 
-        double robotError;
-        double robotHeading = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
-
-
-        if (robotHeading < 0){
-            robotHeading = robotHeading * -1 + 180;
-        }
-        // calculate error in -179 to +180 range  (
-        robotError = targetAngle - robotHeading;
-
-
-
-        while (robotError > 180)  robotError -= 360;
-        while (robotError <= -180) robotError += 360;
-        return robotError;
-    }
 
     /**
-     * returns desired steering force.  +/- 1 range.  +ve = steer left
-     * @param error   Error angle in robot relative degrees
-     * @param PCoeff  Proportional Gain Coefficient
-     * @return
+     * @param gyroTarget The target heading in degrees, between 0 and 360
+     * @param gyroRange  The acceptable range off target in degrees, usually 1 or 2
+     * @param gyroActual The current heading in degrees, between 0 and 360
+     * @param minSpeed   The minimum power to apply in order to turn (e.g. 0.05 when moving or 0.15 when stopped)
+     * @param addSpeed   The maximum additional speed to apply in order to turn (proportional component), e.g. 0.3
      */
-    public double getSteer(double error, double PCoeff) {
-        return Range.clip(error * PCoeff, -1, 1);
+
+    public double gyroCorrect(double gyroTarget, double gyroRange, double gyroActual, double minSpeed, double addSpeed) {
+        double delta = (gyroTarget - gyroActual + 360.0) % 360.0; //the difference between target and actual mod 360
+        if (delta > 180.0) delta -= 360.0; //makes delta between -180 and 180
+        if (Math.abs(delta) > gyroRange) { //checks if delta is out of range
+            double gyroMod = delta / 45.0; //scale from -1 to 1 if delta is less than 45 degrees
+            if (Math.abs(gyroMod) > 1.0)
+                gyroMod = Math.signum(gyroMod); //set gyromod to 1 or -1 if the error is more than 45 degrees
+            return (minSpeed * Math.signum(gyroMod) + addSpeed * gyroMod);
+        } else {
+            return 0.0;
+        }
     }
+
 }
 
