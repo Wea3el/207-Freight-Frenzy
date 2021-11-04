d03e31990e56200e49d7fa83ce01c6aa67f98310
Hrnn55 <bearbrickshiu@gmail.com>
2021-11-04 17:06:31 -0400

    test joe mama iteration 42.0 (added robot class called GeckoBot and mirrored version of BlueEncoderThings, RedEncoderThings)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GeckoBot.java
@@ -0,0 +1,270 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+
+import java.io.File;
+import java.util.List;
+
+public class GeckoBot
+{
+    private HardwareMap hardwareMap;
+    private Telemetry tele;
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    private DcMotor frontLeft;
+    private DcMotor intake;
+    private DcMotor duck; // duck
+    private DcMotor lift;
+
+    private Servo spit;
+
+    BNO055IMU imu;
+    BNO055IMU.Parameters parameters;
+
+    private double power;
+    private double strafePower;
+    private double speed;
+
+    final double     HEADING_THRESHOLD       = 5;      // As tight as we can make it with an integer gyro
+
+    int auto = 0;
+    int place;
+    private int pos;
+
+    boolean calib;
+    boolean detected = false;
+
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
+    private static final String[] LABELS = {
+            "Cube",
+            "Marker"
+    };
+
+    private static final String VUFORIA_KEY =
+            "Acigj+7/////AAABmQugQe1ZQUopr3xiuORZW/1dG/ikPkZLNbC+QPCu21Rzj31nDeEOYFET8+CME7vkvCuEivLG3UEE7YDlkmimZWdyaxfOU2irEatGpMI6l5Pav7W9omY4k2gQcvOA47ml11AyUBomtPu/WUqbJUaviOswXkLMiHt7B7azcyuj/BI2b8Wdc3edXpraMkE++0sZdxn2cPH5lA2A+8CEhw4ogvpwjLiVVVxyQ5QYb6zN5FBVM1E/hpoEEgPCs89h6Plfn3AjbXu+1TEYKTLVgqEvdTlSpbni+5QOqcBHp7m9lXEfjF1OkIqOf1daD11VT0sCa/ZmTaZfgMUKa0p0NuTuCCJH1QwObC0oJFOkrd+h1PZU";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+    public void init() {
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        parameters = new BNO055IMU.Parameters();
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        //parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+
+        telemetry.addLine("Ready.");
+        telemetry.update();
+
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        duck = hardwareMap.get(DcMotor.class, "duck");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        spit = hardwareMap.get(Servo.class, "spit");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER );
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER );
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER );
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER );
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER );
+
+        pos = 0;
+
+        imu.initialize(parameters);
+        initVuforia();
+        initTfod();
+        spit.setPosition(0);
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        tfod.activate();
+
+        // The TensorFlow software will scale the input images from the camera to a lower resolution.
+        // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+        // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+        // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+        // should be set to the value of the images used to create the TensorFlow Object Detection model
+        // (typically 16/9).
+        tfod.setZoom(1.0, 16.0/9.0);
+    }
+
+    private void drive(int targetPos, double speed)
+    {
+        int pos = cmToTicks(targetPos);
+
+        frontLeft.setTargetPosition(pos);
+        backLeft.setTargetPosition(pos);
+        frontRight.setTargetPosition(pos);
+        backRight.setTargetPosition(pos);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(speed);
+        backLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backRight.setPower(speed);
+    }
+    private void lift(int targetPos, double speed)
+    {
+        int pos = targetPos;
+
+        lift.setTargetPosition(pos);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setPower(speed);
+    }
+    private void strafe(int targetPos, double speed, int direction)//true == 1, false == left
+    {
+        int pos = cmToTicks(targetPos) * direction;
+
+        frontLeft.setTargetPosition(pos);
+        backLeft.setTargetPosition(-pos);
+        frontRight.setTargetPosition(-pos);
+        backRight.setTargetPosition(pos);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(speed);
+        backLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backRight.setPower(speed);
+    }
+
+    boolean onHeading(double angle) {
+        double actual = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        double   steer ;
+        boolean  onTarget = false ;
+        double leftSpeed;
+        double rightSpeed;
+        double error =  (angle - actual)-180;
+
+        // determine turn power based on +/- error
+        if (Math.abs(error) <= HEADING_THRESHOLD) {
+            steer = 0.0;
+            leftSpeed  = 0.0;
+            rightSpeed = 0.0;
+            onTarget = true;
+        }
+        else {
+            steer = gyroCorrect(angle, 1, actual , 0.05, 0.2);
+            rightSpeed  = steer;
+            leftSpeed   = -rightSpeed;
+        }
+
+        // Send desired speeds to motors.
+        frontLeft.setPower(leftSpeed);
+        backLeft.setPower(leftSpeed);
+        frontRight.setPower(rightSpeed);
+        backRight.setPower(rightSpeed);
+
+        // Display it for the driver.
+        telemetry.addData("error", error);
+        telemetry.addData("speed", steer);
+        return onTarget;
+    }
+
+    public double gyroCorrect(double gyroTarget, double gyroRange, double gyroActual, double minSpeed, double addSpeed) {
+        double delta = (gyroTarget - gyroActual + 360.0) % 360.0; //the difference between target and actual mod 360
+        if (delta > 180.0) delta -= 360.0; //makes delta between -180 and 180
+        if (Math.abs(delta) > gyroRange) { //checks if delta is out of range
+            double gyroMod = delta / 45.0; //scale from -1 to 1 if delta is less than 45 degrees
+            if (Math.abs(gyroMod) > 1.0)
+                gyroMod = Math.signum(gyroMod); //set gyromod to 1 or -1 if the error is more than 45 degrees
+            return (minSpeed * Math.signum(gyroMod) + addSpeed * gyroMod);
+        } else {
+            return 0.0;
+        }
+    }
+
+    public int cmToTicks(double cm){
+        int ticksPerRev = 537;
+        int diameter = 10;
+        return (int) (ticksPerRev *(cm  / (diameter* Math.PI)));
+    }
+
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.1f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+    }
+}
