04f2aa643a3478baa31bea2fdd5d0f4226ae6c48
Wesley Zhuang <wzhuang22@d-e.org>
2021-11-22 16:02:03 -0500

    ehe camera (OPen Cv code implementation probably doesnt work)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/VisionTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.OpenCV.GripPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class VisionTest extends OpMode {
+    private OpenCvWebcam webcam;
+
+    @Override
+    public void init() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "pp2"), cameraMonitorViewId);
+
+        webcam.setPipeline(new GripPipeline());
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode){ }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Frame Count", webcam.getFrameCount());
+        telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+        telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+        telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+        telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+        telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+        telemetry.update();
+
+        if(gamepad1.a){
+            webcam.stopStreaming();
+        }
+    }
+}
