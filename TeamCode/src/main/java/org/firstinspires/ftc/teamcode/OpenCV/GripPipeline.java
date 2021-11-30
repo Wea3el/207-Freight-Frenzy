63ff0444c884959e91ea7cc165eb1ae0c3741e0d
Wesley Zhuang <wzhuang22@d-e.org>
2021-11-30 09:18:56 -0500

    Vision no worko sadge :( (Added grip files and attempted to implement EasyOpenCv but failed)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/GripPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/GripPipeline.java
@@ -1,13 +1,21 @@
 package org.firstinspires.ftc.teamcode.OpenCV;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+
 import org.opencv.core.*;
 import org.opencv.core.Core.*;
+
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.*;
 import org.opencv.objdetect.*;
 import org.openftc.easyopencv.OpenCvPipeline;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * GripPipeline class.
@@ -19,91 +27,61 @@ import java.util.List;
 public class GripPipeline extends OpenCvPipeline {
 
     //Outputs
-    private Mat cvResizeOutput = new Mat();
-    private Mat blurOutput = new Mat();
     private Mat cvCvtcolorOutput = new Mat();
-    private Mat hsvThresholdOutput = new Mat();
+    private Mat hslThresholdOutput = new Mat();
+    private Mat cvErodeOutput = new Mat();
+    private Mat cvDilateOutput = new Mat();
     private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
-    private ArrayList<MatOfPoint> convexHullsOutput = new ArrayList<MatOfPoint>();
-    private ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
 
+
+    @Override
+    public Mat processFrame(Mat input) {
+        this.process(input);
+        return this.hslThresholdOutput();
+    }
     /**
      * This is the primary method that runs the entire pipeline and updates the outputs.
      */
     public void process(Mat source0) {
-        // Step CV_resize0:
-        Mat cvResizeSrc = source0;
-        Size cvResizeDsize = new Size(0, 0);
-        double cvResizeFx = 0.25;
-        double cvResizeFy = 0.25;
-        int cvResizeInterpolation = Imgproc.INTER_LINEAR;
-        cvResize(cvResizeSrc, cvResizeDsize, cvResizeFx, cvResizeFy, cvResizeInterpolation, cvResizeOutput);
-
-        // Step Blur0:
-        Mat blurInput = cvResizeOutput;
-        BlurType blurType = BlurType.get("Gaussian Blur");
-        double blurRadius = 5.8558558558558556;
-        blur(blurInput, blurType, blurRadius, blurOutput);
-
         // Step CV_cvtColor0:
-        Mat cvCvtcolorSrc = blurOutput;
-        int cvCvtcolorCode = Imgproc.COLOR_HSV2RGB;
+        Mat cvCvtcolorSrc = source0;
+        int cvCvtcolorCode = Imgproc.COLOR_Lab2LBGR;
         cvCvtcolor(cvCvtcolorSrc, cvCvtcolorCode, cvCvtcolorOutput);
 
-        // Step HSV_Threshold0:
-        Mat hsvThresholdInput = cvCvtcolorOutput;
-        double[] hsvThresholdHue = {24.576271186440678, 74.70761621434175};
-        double[] hsvThresholdSaturation = {126.13705645652969, 208.4090909090909};
-        double[] hsvThresholdValue = {145.26836158192089, 215.22727272727272};
-        hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, hsvThresholdOutput);
+        // Step HSL_Threshold0:
+        Mat hslThresholdInput = cvCvtcolorOutput;
+        double[] hslThresholdHue = {0.0, 31.02389078498294};
+        double[] hslThresholdSaturation = {202.4120050107129, 255.0};
+        double[] hslThresholdLuminance = {0.0, 255.0};
+        hslThreshold(hslThresholdInput, hslThresholdHue, hslThresholdSaturation, hslThresholdLuminance, hslThresholdOutput);
+
+        // Step CV_erode0:
+        Mat cvErodeSrc = hslThresholdOutput;
+        Mat cvErodeKernel = new Mat();
+        Point cvErodeAnchor = new Point(-1, -1);
+        double cvErodeIterations = 5.0;
+        int cvErodeBordertype = Core.BORDER_CONSTANT;
+        Scalar cvErodeBordervalue = new Scalar(-1);
+        cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, cvErodeOutput);
+
+        // Step CV_dilate0:
+        Mat cvDilateSrc = cvErodeOutput;
+        Mat cvDilateKernel = new Mat();
+        Point cvDilateAnchor = new Point(-1, -1);
+        double cvDilateIterations = 10.0;
+        int cvDilateBordertype = Core.BORDER_CONSTANT;
+        Scalar cvDilateBordervalue = new Scalar(-1);
+        cvDilate(cvDilateSrc, cvDilateKernel, cvDilateAnchor, cvDilateIterations, cvDilateBordertype, cvDilateBordervalue, cvDilateOutput);
 
         // Step Find_Contours0:
-        Mat findContoursInput = hsvThresholdOutput;
+        Mat findContoursInput = cvDilateOutput;
         boolean findContoursExternalOnly = false;
         findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
 
-        // Step Convex_Hulls0:
-        ArrayList<MatOfPoint> convexHullsContours = findContoursOutput;
-        convexHulls(convexHullsContours, convexHullsOutput);
-
-        // Step Filter_Contours0:
-        ArrayList<MatOfPoint> filterContoursContours = convexHullsOutput;
-        double filterContoursMinArea = 777.0;
-        double filterContoursMinPerimeter = 0.0;
-        double filterContoursMinWidth = 0.0;
-        double filterContoursMaxWidth = 1000.0;
-        double filterContoursMinHeight = 0.0;
-        double filterContoursMaxHeight = 1000.0;
-        double[] filterContoursSolidity = {0.0, 100.0};
-        double filterContoursMaxVertices = 1000000.0;
-        double filterContoursMinVertices = 0.0;
-        double filterContoursMinRatio = 0.0;
-        double filterContoursMaxRatio = 1000.0;
-        filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
-
-    }
-
-    /**
-     * This method is a generated getter for the output of a CV_resize.
-     *
-     * @return Mat output from CV_resize.
-     */
-    public Mat cvResizeOutput() {
-        return cvResizeOutput;
-    }
-
-    /**
-     * This method is a generated getter for the output of a Blur.
-     *
-     * @return Mat output from Blur.
-     */
-    public Mat blurOutput() {
-        return blurOutput;
     }
 
     /**
      * This method is a generated getter for the output of a CV_cvtColor.
-     *
      * @return Mat output from CV_cvtColor.
      */
     public Mat cvCvtcolorOutput() {
@@ -111,160 +89,117 @@ public class GripPipeline extends OpenCvPipeline {
     }
 
     /**
-     * This method is a generated getter for the output of a HSV_Threshold.
-     *
-     * @return Mat output from HSV_Threshold.
+     * This method is a generated getter for the output of a HSL_Threshold.
+     * @return Mat output from HSL_Threshold.
      */
-    public Mat hsvThresholdOutput() {
-        return hsvThresholdOutput;
+    public Mat hslThresholdOutput() {
+        return hslThresholdOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_erode.
+     * @return Mat output from CV_erode.
+     */
+    public Mat cvErodeOutput() {
+        return cvErodeOutput;
+    }
+
+
+
+    /**
+     * This method is a generated getter for the output of a CV_dilate.
+     * @return Mat output from CV_dilate.
+     */
+    public Mat cvDilateOutput() {
+        return cvDilateOutput;
     }
 
     /**
      * This method is a generated getter for the output of a Find_Contours.
-     *
      * @return ArrayList<MatOfPoint> output from Find_Contours.
      */
     public ArrayList<MatOfPoint> findContoursOutput() {
         return findContoursOutput;
     }
 
-    /**
-     * This method is a generated getter for the output of a Convex_Hulls.
-     *
-     * @return ArrayList<MatOfPoint> output from Convex_Hulls.
-     */
-    public ArrayList<MatOfPoint> convexHullsOutput() {
-        return convexHullsOutput;
-    }
-
-    /**
-     * This method is a generated getter for the output of a Filter_Contours.
-     *
-     * @return ArrayList<MatOfPoint> output from Filter_Contours.
-     */
-    public ArrayList<MatOfPoint> filterContoursOutput() {
-        return filterContoursOutput;
-    }
-
-
-    /**
-     * Resizes an image.
-     *
-     * @param src           The image to resize.
-     * @param dSize         size to set the image.
-     * @param fx            scale factor along X axis.
-     * @param fy            scale factor along Y axis.
-     * @param interpolation type of interpolation to use.
-     * @param dst           output image.
-     */
-    private void cvResize(Mat src, Size dSize, double fx, double fy, int interpolation,
-                          Mat dst) {
-        if (dSize == null) {
-            dSize = new Size(0, 0);
-        }
-        Imgproc.resize(src, dst, dSize, fx, fy, interpolation);
-    }
-
-    @Override
-    public Mat processFrame(Mat input) {
-        this.process(input);
-        return this.hsvThresholdOutput;
-    }
-
-    /**
-     * An indication of which type of filter to use for a blur.
-     * Choices are BOX, GAUSSIAN, MEDIAN, and BILATERAL
-     */
-    enum BlurType {
-        BOX("Box Blur"), GAUSSIAN("Gaussian Blur"), MEDIAN("Median Filter"),
-        BILATERAL("Bilateral Filter");
-
-        private final String label;
-
-        BlurType(String label) {
-            this.label = label;
-        }
-
-        public static BlurType get(String type) {
-            if (BILATERAL.label.equals(type)) {
-                return BILATERAL;
-            } else if (GAUSSIAN.label.equals(type)) {
-                return GAUSSIAN;
-            } else if (MEDIAN.label.equals(type)) {
-                return MEDIAN;
-            } else {
-                return BOX;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return this.label;
-        }
-    }
-
-    /**
-     * Softens an image using one of several filters.
-     *
-     * @param input        The image on which to perform the blur.
-     * @param type         The blurType to perform.
-     * @param doubleRadius The radius for the blur.
-     * @param output       The image in which to store the output.
-     */
-    private void blur(Mat input, BlurType type, double doubleRadius,
-                      Mat output) {
-        int radius = (int) (doubleRadius + 0.5);
-        int kernelSize;
-        switch (type) {
-            case BOX:
-                kernelSize = 2 * radius + 1;
-                Imgproc.blur(input, output, new Size(kernelSize, kernelSize));
-                break;
-            case GAUSSIAN:
-                kernelSize = 6 * radius + 1;
-                Imgproc.GaussianBlur(input, output, new Size(kernelSize, kernelSize), radius);
-                break;
-            case MEDIAN:
-                kernelSize = 2 * radius + 1;
-                Imgproc.medianBlur(input, output, kernelSize);
-                break;
-            case BILATERAL:
-                Imgproc.bilateralFilter(input, output, -1, radius, radius);
-                break;
-        }
-    }
 
     /**
      * Converts an image from one color space to another.
-     *
-     * @param src  Image to convert.
+     * @param src Image to convert.
      * @param code conversion code.
-     * @param dst  converted Image.
+     * @param dst converted Image.
      */
     private void cvCvtcolor(Mat src, int code, Mat dst) {
         Imgproc.cvtColor(src, dst, code);
     }
 
     /**
-     * Segment an image based on hue, saturation, and value ranges.
+     * Segment an image based on hue, saturation, and luminance ranges.
      *
-     * @param input  The image on which to perform the HSL threshold.
-     * @param hue    The min and max hue
-     * @param sat    The min and max saturation
-     * @param val    The min and max value
+     * @param input The image on which to perform the HSL threshold.
+     * @param hue The min and max hue
+     * @param sat The min and max saturation
+     * @param lum The min and max luminance
      * @param out The image in which to store the output.
      */
-    private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
+    private void hslThreshold(Mat input, double[] hue, double[] sat, double[] lum,
                               Mat out) {
-        Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
-        Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
-                new Scalar(hue[1], sat[1], val[1]), out);
+        Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HLS);
+        Core.inRange(out, new Scalar(hue[0], lum[0], sat[0]),
+                new Scalar(hue[1], lum[1], sat[1]), out);
+    }
+
+    /**
+     * Expands area of lower value in an image.
+     * @param src the Image to erode.
+     * @param kernel the kernel for erosion.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the erosion.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvErode(Mat src, Mat kernel, Point anchor, double iterations,
+                         int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null) {
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.erode(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+    }
+
+    /**
+     * Expands area of higher value in an image.
+     * @param src the Image to dilate.
+     * @param kernel the kernel for dilation.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the dilation.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvDilate(Mat src, Mat kernel, Point anchor, double iterations,
+                          int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null){
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.dilate(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
     }
 
     /**
      * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
-     *
-     * @param input    The image on which to perform the Distance Transform.
+     * @param input The image on which to perform the Distance Transform.
 
      */
     private void findContours(Mat input, boolean externalOnly,
@@ -274,87 +209,18 @@ public class GripPipeline extends OpenCvPipeline {
         int mode;
         if (externalOnly) {
             mode = Imgproc.RETR_EXTERNAL;
-        } else {
+        }
+        else {
             mode = Imgproc.RETR_LIST;
         }
         int method = Imgproc.CHAIN_APPROX_SIMPLE;
         Imgproc.findContours(input, contours, hierarchy, mode, method);
     }
 
-    /**
-     * Compute the convex hulls of contours.
-     *
-     * @param inputContours  The contours on which to perform the operation.
-     * @param outputContours The contours where the output will be stored.
-     */
-    private void convexHulls(List<MatOfPoint> inputContours,
-                             ArrayList<MatOfPoint> outputContours) {
-        final MatOfInt hull = new MatOfInt();
-        outputContours.clear();
-        for (int i = 0; i < inputContours.size(); i++) {
-            final MatOfPoint contour = inputContours.get(i);
-            final MatOfPoint mopHull = new MatOfPoint();
-            Imgproc.convexHull(contour, hull);
-            mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
-            for (int j = 0; j < hull.size().height; j++) {
-                int index = (int) hull.get(j, 0)[0];
-                double[] point = new double[]{contour.get(index, 0)[0], contour.get(index, 0)[1]};
-                mopHull.put(j, 0, point);
-            }
-            outputContours.add(mopHull);
-        }
-    }
 
 
-    /**
-     * Filters out contours that do not meet certain criteria.
-     *
-     * @param inputContours  is the input list of contours
-     * @param output         is the the output list of contours
-     * @param minArea        is the minimum area of a contour that will be kept
-     * @param minPerimeter   is the minimum perimeter of a contour that will be kept
-     * @param minWidth       minimum width of a contour
-     * @param maxWidth       maximum width
-     * @param minHeight      minimum height
-     * @param maxHeight      maximimum height
-     * @param solidity       the minimum and maximum solidity of a contour
-     * @param minVertexCount minimum vertex Count of the contours
-     * @param maxVertexCount maximum vertex Count
-     * @param minRatio       minimum ratio of width to height
-     * @param maxRatio       maximum ratio of width to height
-     */
-    private void filterContours(List<MatOfPoint> inputContours, double minArea,
-                                double minPerimeter, double minWidth, double maxWidth, double minHeight, double
-                                        maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double
-                                        minRatio, double maxRatio, List<MatOfPoint> output) {
-        final MatOfInt hull = new MatOfInt();
-        output.clear();
-        //operation
-        for (int i = 0; i < inputContours.size(); i++) {
-            final MatOfPoint contour = inputContours.get(i);
-            final Rect bb = Imgproc.boundingRect(contour);
-            if (bb.width < minWidth || bb.width > maxWidth) continue;
-            if (bb.height < minHeight || bb.height > maxHeight) continue;
-            final double area = Imgproc.contourArea(contour);
-            if (area < minArea) continue;
-            if (Imgproc.arcLength(new MatOfPoint2f(contour.toArray()), true) < minPerimeter) continue;
-            Imgproc.convexHull(contour, hull);
-            MatOfPoint mopHull = new MatOfPoint();
-            mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
-            for (int j = 0; j < hull.size().height; j++) {
-                int index = (int) hull.get(j, 0)[0];
-                double[] point = new double[]{contour.get(index, 0)[0], contour.get(index, 0)[1]};
-                mopHull.put(j, 0, point);
-            }
-            final double solid = 100 * area / Imgproc.contourArea(mopHull);
-            if (solid < solidity[0] || solid > solidity[1]) continue;
-            if (contour.rows() < minVertexCount || contour.rows() > maxVertexCount) continue;
-            final double ratio = bb.width / (double) bb.height;
-            if (ratio < minRatio || ratio > maxRatio) continue;
-            output.add(contour);
-        }
-    }
 
 
 }
+
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/VisionTest.java
@@ -1,27 +1,39 @@
 package org.firstinspires.ftc.teamcode.OpenCV;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.OpenCV.GripPipeline;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 
+import java.util.List;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="camera", group="Iterative Opmode")
 public class VisionTest extends OpMode {
     private OpenCvWebcam webcam;
+    private DetectionLevel level;
+    private GripPipeline grip;
+
+    private static final int CAMERA_WIDTH = 1280;
 
     @Override
     public void init() {
+        grip = new GripPipeline();
+
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "pp2"), cameraMonitorViewId);
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "pp"), cameraMonitorViewId);
 
-        webcam.setPipeline(new GripPipeline());
+        webcam.setPipeline(grip);
         webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
-
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
                 webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
@@ -30,23 +42,68 @@ public class VisionTest extends OpMode {
             @Override
             public void onError(int errorCode){ }
         });
-
-        telemetry.addLine("Waiting for start");
+        telemetry.addData("status", "initialized");
         telemetry.update();
     }
 
     @Override
     public void loop() {
-        telemetry.addData("Frame Count", webcam.getFrameCount());
-        telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
-        telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
-        telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
-        telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
-        telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
-        telemetry.update();
 
-        if(gamepad1.a){
-            webcam.stopStreaming();
+        telemetry.update();
+    }
+
+    private void updateDetermination(){
+        List<MatOfPoint> hulls = grip.findContoursOutput();
+        for(int i = 0; i < hulls.size(); i++){
+            Rect boundingRect = Imgproc.boundingRect(hulls.get(i));
+            if(boundingRect.area() < 20){
+                hulls.remove(i);
+                i--;
+            }
         }
+
+        if(hulls.size() == 0){
+            level = DetectionLevel.LEVEL_THREE;
+        }else{
+            hulls.size();
+            double areaSum = 0;
+            double xAvg = 0;
+            //double yAvg = 0;
+            for (int i = 0; i < hulls.size(); i++) {
+                Rect boundingRect = Imgproc.boundingRect(hulls.get(i));
+                double areaTotal = boundingRect.area();
+                areaSum = areaSum + areaTotal;
+                xAvg = xAvg + areaTotal * (boundingRect.x + boundingRect.width / 2d);
+                //yAvg = yAvg + areaLol*(boundingRect.y + boundingRect.height / 2d);
+            }
+            xAvg = xAvg / areaSum;
+
+            if (xAvg > CAMERA_WIDTH/2) {
+                this.level = DetectionLevel.LEVEL_TWO;
+            } else {
+                this.level = DetectionLevel.LEVEL_ONE;
+            }
+        }
+    }
+
+    public DetectionLevel currentDetermination(){
+        updateDetermination();
+        return this.level;
+    }
+
+    public void stop(){  }
+
+    public enum DetectionLevel{
+        LEVEL_ONE("1 - Lowest level"), LEVEL_TWO("2 - Second level"),
+        LEVEL_THREE("3 - Top level"), UNKNOWN("Unknown");
+
+        private final String level;
+
+        DetectionLevel(String str) {
+            this.level = str;
+        }
+
+        @Override
+        public String toString() { return this.level; }
     }
 }
