5498cdf697794c8bf7355fb820b502881fa88bd3
Wesley Zhuang <wzhuang22@d-e.org>
2021-12-14 17:09:07 -0500

    robot class setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Intake {
+    //declaring variables for this file
+    private DcMotor intake;
+
+    //constructor to set directions, power modes, and hardware map
+    public Intake(HardwareMap map) {
+        intake = map.get(DcMotor.class, "intake");
+
+    }
+
+    //different methods to do whatever needs to be done for this file, in this case, only intake and outtake
+    public void intake(){
+
+    }
+    public void outake(){
+
+    }
 }
