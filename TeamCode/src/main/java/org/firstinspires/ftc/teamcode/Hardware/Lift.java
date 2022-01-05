6da63882e4978320f80783a1f1a49506a69497df
amiller23 <amiller23@d-e.org>
2022-01-05 16:39:21 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Lift.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Lift {
-}
+    private DcMotor slideLeft;
+    private DcMotor slideRight;
+    private Servo gateIn;
+    private Servo gateOut;
+    private Servo slope;
+
+    //constructor
+    public Lift(HardwareMap map) {
+        slideLeft = map.get(DcMotor.class, "SL");
+        slideRight = map.get(DcMotor.class, "SR");
+        gateIn = map.get(Servo.class, "GI");
+        gateOut = map.get(Servo.class, "GO");
+        slope = map.get(Servo.class, "S");
+        //set position, reverse one lift motor's direction
+
+        slideLeft.setDirection(DcMotor.Direction.REVERSE);
+        slideRight.setDirection(DcMotor.Direction.FORWARD);
+
+        gateIn.setPosition(0);
+        gateOut.setPosition(0);
+        slope.setPosition(0);
+
+    }
+
+
+    }
+
